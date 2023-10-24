fec608be64dae3094898870720405820c4997190
Miguel Angel Luna Colmenares <42624380+izZikA@users.noreply.github.com>
2023-10-24 09:21:22 -0600

    Update programa.py

--- programa.py
+++ programa.py
@@ -1,8 +1,29 @@
 import socket
 import threading
 import time
+import subprocess
+
+
 
 mensajes_para_guardar = []
+interfaz = "ens33"  # Reemplaza con el nombre de la interfaz que deseas consultar
+
+def obtener_direccion_ip(interface):
+    try:
+        # Ejecutar el comando 'ip addr' y capturar la salida
+        resultado = subprocess.check_output(['ip', 'addr', 'show', interface]).decode('utf-8')
+
+        # Buscar la línea que contiene 'inet' en la salida del comando
+        for linea in resultado.split('\n'):
+            if 'inet' in linea:
+                partes = linea.strip().split()
+                inet_index = partes.index('inet')
+                direccion_ip = partes[inet_index + 1].split('/')[0]
+                return direccion_ip
+
+    except subprocess.CalledProcessError:
+        return "No se pudo obtener la dirección IP"
+
 
 def recibir_mensajes():
     mensaje_confirmado = False
@@ -49,7 +70,11 @@ def enviar_mensajes():
 
 # Configura la dirección y el puerto en esta máquina virtual
 # Se cambia conforme el ip de la maquina virtual
-mi_ip = "192.168.253.129"
+mi_ip = obtener_direccion_ip(interfaz)
+   if mi_ip:
+        print(f"La dirección IP (inet) de la interfaz {interfaz} es: {direccion_ip}")
+    else:
+        print("No se pudo obtener la dirección IP.")
 mi_puerto = 12345 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
