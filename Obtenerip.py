8a6f443865cb31e43752c36b2d865889c3d00c68
Miguel Angel Luna Colmenares <42624380+izZikA@users.noreply.github.com>
2023-10-24 08:33:53 -0600

    Update Obtenerip.py

--- Obtenerip.py
+++ Obtenerip.py
@@ -1,15 +1,26 @@
-import socket
+import subprocess
 
-def obtener_direccion_ip():
+def obtener_direccion_ip(interface):
     try:
-        
-        # Obtenemos la dirección IP asociada al nombre del host
-        direccion_ip = socket.gethostbyname(ens33)
-        return direccion_ip
-    except socket.error as e:
-        return str(e)
+        # Ejecutar el comando 'ip addr' y capturar la salida
+        resultado = subprocess.check_output(['ip', 'addr', 'show', interface]).decode('utf-8')
 
-# Llamamos a la función para obtener la dirección IP y la imprimimos
-ip = obtener_direccion_ip()
-print(f"La dirección IP de la red local es: {ip}")
+        # Buscar la línea que contiene 'inet' en la salida del comando
+        for linea in resultado.split('\n'):
+            if 'inet' in linea:
+                partes = linea.strip().split()
+                inet_index = partes.index('inet')
+                direccion_ip = partes[inet_index + 1].split('/')[0]
+                return direccion_ip
 
+    except subprocess.CalledProcessError:
+        return "No se pudo obtener la dirección IP"
+
+if __name__ == "__main__":
+    interfaz = "eth0"  # Reemplaza con el nombre de la interfaz que deseas consultar
+    direccion_ip = obtener_direccion_ip(interfaz)
+
+    if direccion_ip:
+        print(f"La dirección IP (inet) de la interfaz {interfaz} es: {direccion_ip}")
+    else:
+        print("No se pudo obtener la dirección IP.")
