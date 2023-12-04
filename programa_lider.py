989b7607c22b9b6a10add8b6ae6b0bb4d0d26bd1
Miguel Angel Luna Colmenares <42624380+izZikA@users.noreply.github.com>
2023-12-03 23:11:45 -0600

    Update programa_lider.py

--- programa_lider.py
+++ programa_lider.py
@@ -3,28 +3,29 @@ import threading
 import time
 import subprocess
 
-lista_de_nodos = ["192.168.192.13"]  # Ejemplo
-estado_de_nodos = {nodo: False for nodo in lista_de_nodos}  # False significa 'no confirmado'
-
 
 
 mensajes_para_guardar = []
+registro_actividad = {} 
+lista_nodos = ["192.168.192."]
 
-def seleccionar_lider(direccion_nueva, lider_actual):
-    if not lider_actual or direccion_nueva > lider_actual:
-        return direccion_nueva
-    return lider_actual
-
-
-
-def enviar_heartbeats():
+def enviar_heartbeat():
     while True:
-        for nodo in lista_de_nodos:
-            try:
-                s.sendto("heartbeat".encode('utf-8'), (nodo, mi_puerto))
-            except Exception as e:
-                print(f"No se pudo enviar heartbeat a {nodo}: {e}")
-        time.sleep(5)  # Enviar heartbeats cada 5 segundos, por ejemplo
+        mensaje_heartbeat = "HEARTBEAT"
+        for destino_ip in lista_nodos:  # Asegúrate de definir esta lista
+            s.sendto(mensaje_heartbeat.encode('utf-8'), (destino_ip, mi_puerto))
+        time.sleep(5)  # Intervalo de tiempo entre heartbeats (ajustar según sea necesario)
+
+
+def monitorear_nodos():
+    while True:
+        tiempo_actual = time.time()
+        for nodo, ultimo_heartbeat in list(registro_actividad.items()):
+            if tiempo_actual - ultimo_heartbeat > 10:  # Umbral de tiempo (ajustar según sea necesario)
+                print(f"Nodo {nodo} no responde")
+                del registro_actividad[nodo]
+        time.sleep(5)  # Intervalo de tiempo para el monitoreo
+
 
 
 
@@ -50,11 +51,10 @@ def recibir_mensajes():
     while True:
         try:
             mensaje_recibido, direccion = s.recvfrom(1024)
-            mensaje_decodificado = mensaje_recibido.decode('utf-8')
-
-            if mensaje_decodificado == "heartbeat":
-                estado_de_nodos[direccion[0]] = True
+            if mensaje_recibido.decode('utf-8') == "HEARTBEAT":
+                registro_actividad[direccion] = time.time()
             else:
+                mensaje_decodificado = mensaje_recibido.decode('utf-8')
                 timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
                 mensaje_completo = f"{timestamp} - Mensaje RECIBIDO de {direccion}: {mensaje_decodificado}"
                 mensajes_para_guardar.append(mensaje_completo)
@@ -92,26 +92,6 @@ def enviar_mensajes():
         mensajes_para_guardar.append(mensaje_completo)
 
 
-def verificar_nodos():
-    while True:
-        # Espera un tiempo suficiente para recibir heartbeats
-        time.sleep(6)
-
-        # Restablece todos los estados de nodos a False
-        for nodo in lista_de_nodos:
-            estado_de_nodos[nodo] = False
-
-        # Espera un poco para dar tiempo a los heartbeats de llegar
-        time.sleep(1)
-
-        # Ahora verifica y reporta el estado actualizado de cada nodo
-        for nodo in lista_de_nodos:
-            if estado_de_nodos[nodo]:
-                print(f"Nodo {nodo} está vivo.")
-            else:
-                print(f"Nodo {nodo} podría estar caído.")
-
-
 # Configura la dirección y el puerto en esta máquina virtual
 # Se cambia conforme el ip de la maquina virtual
 interfaz = "ens33"  # Reemplaza con el nombre de la interfaz que deseas consultar
@@ -126,31 +106,22 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind((mi_ip, mi_puerto))
 s.settimeout(1)
 
-thread_enviar_heartbeats = threading.Thread(target=enviar_heartbeats)
-thread_enviar_heartbeats.daemon = True
-thread_enviar_heartbeats.start()
-
-thread_verificar_nodos = threading.Thread(target=verificar_nodos)
-thread_verificar_nodos.daemon = True
-thread_verificar_nodos.start()
-
-
 # Crea hilos para recibir, enviar y guardar mensajes
+
 thread_recibir = threading.Thread(target=recibir_mensajes)
 thread_enviar = threading.Thread(target=enviar_mensajes)
 thread_guardar = threading.Thread(target=guardar_mensajes)
-
+thread_heartbeat = threading.Thread(target=enviar_heartbeat)
+thread_monitorear = threading.Thread(target=monitorear_nodos)
 # Que corran en segundo plano
 thread_recibir.daemon = True
 thread_enviar.daemon = True
 thread_guardar.daemon = True
 
-# Los inicializa
-thread_recibir.start()
-thread_enviar.start()
-thread_guardar.start()
+for thread in [thread_recibir, thread_enviar, thread_guardar, thread_heartbeat, thread_monitorear]:
+    thread.daemon = True
+    thread.start()
 
 # El programa principal no hace nada más que esperar
 while True:
     pass
-
