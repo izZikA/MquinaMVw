77b397b5ecfe60aae11a0ea37c2dbcffc232841e
Miguel Angel Luna Colmenares <42624380+izZikA@users.noreply.github.com>
2023-12-03 19:58:16 -0600

    Update programa_lider.py

--- programa_lider.py
+++ programa_lider.py
@@ -3,63 +3,62 @@ import threading
 import time
 import subprocess
 
-# Variables globales
-mensajes_para_guardar = []
-nodos_conocidos = set()
-lider = None
-mi_ip = None
-mi_puerto = 12345
 
-# Funciones definidas previamente
+
+mensajes_para_guardar = []
+nodos_ips = []
+
+
+def elegir_lider_y_anunciar():
+    global nodos_ips
+    if nodos_ips:
+        lider = max(nodos_ips)
+        mensaje_lider = f"Ahora el nuevo lider es {lider}"
+        print(mensaje_lider)
+        for ip in nodos_ips:
+            s.sendto(mensaje_lider.encode('utf-8'), (ip, 12345))
+
 def obtener_direccion_ip(interface):
     try:
+        # Ejecutar el comando 'ip addr' y capturar la salida
         resultado = subprocess.check_output(['ip', 'addr', 'show', interface]).decode('utf-8')
+
+        # Buscar la línea que contiene 'inet' en la salida del comando
         for linea in resultado.split('\n'):
             if 'inet' in linea:
                 partes = linea.strip().split()
                 inet_index = partes.index('inet')
                 direccion_ip = partes[inet_index + 1].split('/')[0]
                 return direccion_ip
+
     except subprocess.CalledProcessError:
-        return None
+        return "No se pudo obtener la dirección IP"
 
-def actualizar_lider():
-    global lider, nodos_conocidos
-    if nodos_conocidos:
-        lider_actual = lider
-        lider = max(nodos_conocidos)
-        if lider_actual != lider:
-            print(f"El nuevo líder es: {lider}")
 
-def enviar_heartbeats(s):
-    global mi_ip, mi_puerto, nodos_conocidos
-    while True:
-        for ip in nodos_conocidos:
-            if ip != mi_ip:
-                s.sendto(f"Heartbeat desde {mi_ip}".encode('utf-8'), (ip, mi_puerto))
-        time.sleep(5)
-
-# Función modificada para recibir mensajes, heartbeats y actualizar el líder
-def recibir_mensajes(s):
-    global mensajes_para_guardar, nodos_conocidos
+def recibir_mensajes():
+    global nodos_ips
+    mensaje_confirmado = False
     while True:
         try:
             mensaje_recibido, direccion = s.recvfrom(1024)
-            ip_nodo, _ = direccion
+            direccion_ip = direccion[0]
+            if direccion_ip not in nodos_ips:
+                nodos_ips.append(direccion_ip)
+                elegir_lider_y_anunciar()
+
             mensaje_decodificado = mensaje_recibido.decode('utf-8')
-            if mensaje_decodificado.startswith("Heartbeat"):
-                nodos_conocidos.add(ip_nodo)
-                actualizar_lider()
-            else:
-                timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-                mensaje_completo = f"{timestamp} - Mensaje RECIBIDO de {direccion}: {mensaje_decodificado}"
-                mensajes_para_guardar.append(mensaje_completo)
-                print(mensaje_completo)
-                # Enviar confirmación de recepción
-                confirmacion = "Mensaje recibido"
+            timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            mensaje_completo = f"{timestamp} - Mensaje RECIBIDO de {direccion}: {mensaje_decodificado}"
+            mensajes_para_guardar.append(mensaje_completo)
+            
+            if not mensaje_confirmado:
+                confirmacion = "Confirmo la recepcion de tu mensaje"
                 s.sendto(confirmacion.encode('utf-8'), direccion)
+                print(mensaje_completo)
+                mensaje_confirmado = True
         except socket.timeout:
-            pass
+            mensaje_confirmado = False
+
 
 def guardar_mensajes():
     while True:
@@ -67,55 +66,53 @@ def guardar_mensajes():
             mensaje_para_guardar = mensajes_para_guardar.pop(0)
             with open("logMensajes.txt", "a") as log_file:
                 log_file.write(mensaje_para_guardar + "\n")
-            time.sleep(1)
+            time.sleep(1)  # Espera un segundo antes de intentar guardar el siguiente mensaje
 
-def enviar_mensajes(s):
-    global mi_puerto
+def enviar_mensajes():
     while True:
         destino_ip = input("Ingrese la dirección IP de destino: ")
         mensaje = input("Ingrese su mensaje: ")
         timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         mensaje_completo = f"{timestamp} - Mensaje ENVIADO a {destino_ip}: {mensaje}"
-        s.sendto(mensaje.encode('utf-8'), (destino_ip, mi_puerto))
+        #print(mensaje_completo)
+        
+        # Envía el mensaje a la dirección IP de destino especificada
+        destino_puerto = 12345  # Se queda en 12345 para que no haya fallas
+        s.sendto(mensaje.encode('utf-8'), (destino_ip, destino_puerto))
         mensajes_para_guardar.append(mensaje_completo)
 
-# Configuración inicial
-interfaz = "ens33"
+
+# Configura la dirección y el puerto en esta máquina virtual
+# Se cambia conforme el ip de la maquina virtual
+interfaz = "ens33"  # Reemplaza con el nombre de la interfaz que deseas consultar
 mi_ip = obtener_direccion_ip(interfaz)
 if mi_ip:
-    print(f"La dirección IP (inet) de la interfaz {interfaz} es: {mi_ip}")
-    nodos_conocidos.add(mi_ip)
-    lider = mi_ip  # Inicialmente, este nodo se considera líder hasta recibir heartbeats
+   print(f"La dirección IP (inet) de la interfaz {interfaz} es: {mi_ip}")
 else:
     print("No se pudo obtener la dirección IP.")
-    exit(1)
+mi_puerto = 12345 
 
-# Creación y configuración del socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind((mi_ip, mi_puerto))
-s.settimeout(2)
+s.settimeout(1)
 
-# Creación y ejecución de hilos
-thread_recibir = threading.Thread(target=recibir_mensajes, args=(s,))
-thread_enviar = threading.Thread(target=enviar_mensajes, args=(s,))
+# Crea hilos para recibir, enviar y guardar mensajes
+thread_recibir = threading.Thread(target=recibir_mensajes)
+thread_enviar = threading.Thread(target=enviar_mensajes)
 thread_guardar = threading.Thread(target=guardar_mensajes)
-thread_heartbeats = threading.Thread(target=enviar_heartbeats, args=(s,))
 
+# Que corran en segundo plano
 thread_recibir.daemon = True
 thread_enviar.daemon = True
 thread_guardar.daemon = True
-thread_heartbeats.daemon = True
 
+# Los inicializa
 thread_recibir.start()
 thread_enviar.start()
 thread_guardar.start()
-thread_heartbeats.start()
-
 
 # El programa principal no hace nada más que esperar
 while True:
     pass
-
-
 
 
