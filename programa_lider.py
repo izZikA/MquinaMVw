aeed03285a0c2960659ff62f2bfe1885d1b8f14d
Miguel Angel Luna Colmenares <42624380+izZikA@users.noreply.github.com>
2023-12-06 10:22:54 -0600

    Update programa_lider.py

--- programa_lider.py
+++ programa_lider.py
@@ -4,30 +4,20 @@ import time
 import subprocess
 
 # Direcciones IP y puertos de los nodos en la red
-NODES = [("192.168.106.137", 5000), ("192.168.106.135", 5000)]
+NODES = [("192.168.192.1", 5000), ("192.168.192.2", 5000)]  # Asegúrate de ajustar estas direcciones IP
 # Intervalo en segundos para enviar heartbeats
 HEARTBEAT_INTERVAL = 5
 # Tiempo máximo en segundos para considerar un nodo como inactivo
 MAX_INACTIVE_TIME = 15
 
-# Registro de la última vez que se recibió un heartbeat de cada nodo
-last_heartbeat = {}
-
-# Nodo maestro actual
-nodo_maestro = None
-
-# Esta función elige el nodo con la IP más alta como maestro
-def elegir_nodo_maestro(nodos):
-    return max(nodos, key=lambda node: socket.inet_aton(node[0]))
-
 # Esta función envía heartbeats a todos los nodos
 def send_heartbeats():
-    global nodo_maestro
     while True:
+        msg = f"Heartbeat from {mi_ip}"
+        if soy_el_maestro:
+            msg += " | soy el nodo maestro"
         for node in NODES:
             try:
-                # Incluir información del nodo maestro en el mensaje
-                msg = f"Heartbeat from {mi_ip}, master: {nodo_maestro[0]}"
                 sock.sendto(msg.encode(), node)
             except Exception as e:
                 print(f"Error al enviar heartbeat a {node}: {e}")
@@ -35,39 +25,42 @@ def send_heartbeats():
 
 # Esta función recibe heartbeats y actualiza el estado de los nodos
 def receive_heartbeats():
-    global nodo_maestro
-
     while True:
         try:
             data, addr = sock.recvfrom(1024)
+            mensaje = data.decode()
+            print(f"Heartbeat recibido de {addr}, mensaje: {mensaje}")
             last_heartbeat[addr] = time.time()
-            print(f"Heartbeat recibido de {addr}, nodo activo")
-
-            # Verificar si el nodo que envió el heartbeat es nuevo y si tiene la IP más alta
-            if addr not in NODES:
-                NODES.append(addr)
-                print(f"Nuevo nodo detectado: {addr}")
-
-            # Revisar si el nodo recién agregado o actualizado tiene una IP más alta
-            possible_new_master = elegir_nodo_maestro(NODES)
-            if possible_new_master != nodo_maestro:
-                nodo_maestro = possible_new_master
-                print(f"El nodo maestro ha cambiado a {nodo_maestro}")
-
+            if "soy el nodo maestro" in mensaje:
+                actualizar_estado_maestro(addr)
+            elif mi_ip > addr[0] and not hay_maestro_activo():
+                declarar_como_maestro()
         except socket.timeout:
             pass
 
-        # Verificar el estado del nodo maestro
-        verificar_nodo_maestro()
+        # Verificar nodos inactivos
+        verificar_nodos_inactivos()
 
+def actualizar_estado_maestro(addr):
+    global maestro_actual
+    maestro_actual = addr[0]
 
-def verificar_nodo_maestro():
-    global nodo_maestro
+def declarar_como_maestro():
+    global soy_el_maestro
+    soy_el_maestro = True
+    print(f"{mi_ip} se ha declarado como el nodo maestro")
+
+def hay_maestro_activo():
+    return maestro_actual and time.time() - last_heartbeat.get((maestro_actual, 5000), 0) < MAX_INACTIVE_TIME
+
+def verificar_nodos_inactivos():
     current_time = time.time()
-    if nodo_maestro and current_time - last_heartbeat.get(nodo_maestro, 0) > MAX_INACTIVE_TIME:
-        print(f"El nodo maestro {nodo_maestro} ha dejado de estar activo. Reelecting master node.")
-        nodo_maestro = elegir_nodo_maestro([node for node in NODES if node != nodo_maestro])
-        print(f"Nuevo nodo maestro es {nodo_maestro}")
+    for node in NODES:
+        if current_time - last_heartbeat.get(node, 0) > MAX_INACTIVE_TIME:
+            if node[0] == maestro_actual:
+                print(f"El nodo maestro {node} ha dejado de estar activo")
+                maestro_actual = None
+            print(f"{node} ha dejado de estar activo")
 
 def obtener_direccion_ip(interface):
     try:
@@ -80,17 +73,21 @@ def obtener_direccion_ip(interface):
                 return direccion_ip
     except subprocess.CalledProcessError:
         return "No se pudo obtener la dirección IP"
-
+    
+# Inicialización
 interfaz = "ens33"
 mi_ip = obtener_direccion_ip(interfaz)
-
-# Asignar el nodo maestro inicial
-nodo_maestro = elegir_nodo_maestro(NODES)
+soy_el_maestro = False
+maestro_actual = None
 
 # Crear socket UDP
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Asignar IP y puerto local
 sock.bind((mi_ip, 5000))
 sock.settimeout(1)
+
+# Registro de la última vez que se recibió un heartbeat de cada nodo
+last_heartbeat = {}
 
 # Iniciar hilos para enviar y recibir heartbeats
 send_thread = threading.Thread(target=send_heartbeats)
@@ -100,4 +97,3 @@ receive_thread.start()
 
 send_thread.join()
 receive_thread.join()
-
