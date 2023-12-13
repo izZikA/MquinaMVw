57471ae9134ca6b2f152d2ad9d3d7cc7827d0de2
Miguel Angel Luna Colmenares <42624380+izZikA@users.noreply.github.com>
2023-12-13 10:27:36 -0600

    Update programa_lider.py

--- programa_lider.py
+++ programa_lider.py
@@ -6,14 +6,56 @@ import subprocess
 # Direcciones IP y puertos de los nodos en la red
 NODES = [("192.168.192.130", 5000), ("192.168.192.131", 5000),
          ("192.168.192.132", 5000), ("192.168.192.133", 5000)]
-
 # Intervalo en segundos para enviar heartbeats
 HEARTBEAT_INTERVAL = 5
-
 # Tiempo máximo en segundos para considerar un nodo como inactivo
 MAX_INACTIVE_TIME = 15
 
-# Función para obtener la dirección IP de una interfaz
+# Esta función envía heartbeats a todos los nodos
+def send_heartbeats():
+    global master_node
+    while True:
+        heartbeat_msg = "Heartbeat"
+        with master_lock:
+            if (mi_ip, 5000) == master_node:
+                heartbeat_msg += ":Master"
+        for node in NODES:
+            try:
+                sock.sendto(heartbeat_msg.encode(), node)
+            except Exception as e:
+                print(f"Error al enviar heartbeat a {node}: {e}")
+        time.sleep(HEARTBEAT_INTERVAL)
+
+# Esta función recibe heartbeats y actualiza el estado de los nodos
+def receive_heartbeats_modified():
+    global master_node
+    while True:
+        try:
+            data, addr = sock.recvfrom(1024)
+            last_heartbeat[addr] = time.time()
+            if "Master" in data.decode():
+                with master_lock:
+                    if addr != master_node:
+                        print(f"Nuevo nodo maestro elegido: {addr}")
+                        master_node = addr
+            print(f"Heartbeat recibido de {addr}, nodo activo")
+        except socket.timeout:
+            pass
+
+        # Verificar nodos inactivos y actualizar el nodo maestro
+        determine_master()
+
+# Función para determinar el nodo maestro
+def determine_master():
+    global master_node
+    with master_lock:
+        active_nodes = {node: last_heartbeat[node] for node in NODES if time.time() - last_heartbeat.get(node, 0) <= MAX_INACTIVE_TIME}
+        if active_nodes:
+            highest_ip = max(active_nodes.keys())
+            if highest_ip != master_node:
+                master_node = highest_ip
+                print(f"El nodo maestro actual es: {master_node}")
+
 def obtener_direccion_ip(interface):
     try:
         resultado = subprocess.check_output(['ip', 'addr', 'show', interface]).decode('utf-8')
@@ -26,66 +68,30 @@ def obtener_direccion_ip(interface):
     except subprocess.CalledProcessError:
         return "No se pudo obtener la dirección IP"
 
-# Función para determinar el nodo maestro
-def determinar_nodo_maestro():
-    nodos_activos = [node for node, last_time in last_heartbeat.items() 
-                     if time.time() - last_time <= MAX_INACTIVE_TIME]
-    if nodos_activos:
-        return max(nodos_activos, key=lambda node: node[0])
-    return None
-
-# Función que envía heartbeats a todos los nodos
-def send_heartbeats():
-    while True:
-        maestro = determinar_nodo_maestro()
-        es_maestro = (mi_ip, 5000) == maestro
-        mensaje = b"Soy el maestro" if es_maestro else b"Heartbeat"
-
-        for node in NODES:
-            try:
-                sock.sendto(mensaje, node)
-            except Exception as e:
-                print(f"Error al enviar heartbeat a {node}: {e}")
-        time.sleep(HEARTBEAT_INTERVAL)
-
-# Función que recibe heartbeats y actualiza el estado de los nodos
-def receive_heartbeats():
-    while True:
-        try:
-            data, addr = sock.recvfrom(1024)
-            last_heartbeat[addr] = time.time()
-            es_maestro = data == b"Soy el maestro"
-            if es_maestro:
-                print(f"Heartbeat recibido de {addr}, es el nodo maestro")
-            else:
-                print(f"Heartbeat recibido de {addr}, nodo activo")
-        except socket.timeout:
-            pass
-
-        # Verificar nodos inactivos
-        verificar_nodos_inactivos()
-
-def verificar_nodos_inactivos():
-    current_time = time.time()
-    for node in NODES:
-        if current_time - last_heartbeat.get(node, 0) > MAX_INACTIVE_TIME:
-            print(f"{node} ha dejado de estar activo")
-            last_heartbeat[node] = current_time
-
-# Obtener la dirección IP local
+# Obtener dirección IP de la interfaz
 interfaz = "ens33"
 mi_ip = obtener_direccion_ip(interfaz)
 
 # Crear socket UDP
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Asignar IP y puerto local
 sock.bind((mi_ip, 5000))
 sock.settimeout(1)
 
 # Registro de la última vez que se recibió un heartbeat de cada nodo
-last_heartbeat = {node: 0 for node in NODES}
+last_heartbeat = {}
+
+# Lock para control de acceso concurrente al nodo maestro
+master_lock = threading.Lock()
+
+# Nodo maestro actual
+master_node = None
 
 # Iniciar hilos para enviar y recibir heartbeats
 send_thread = threading.Thread(target=send_heartbeats)
-receive_thread = threading.Thread(target=receive_heartbeats)
+receive_thread = threading.Thread(target=receive_heartbeats_modified)
 send_thread.start()
 receive_thread.start()
+
+send_thread.join()
+receive_thread.join()
